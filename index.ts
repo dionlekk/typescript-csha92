ac5d4179d74ba674954812cc052e603b0e33b3aa
dionlekk <74821838+dionlekk@users.noreply.github.com>
2023-04-02 13:34:56 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
 // Import stylesheets
 import './style.css';
 
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+const accounting = new Department('Accounting');
+
+console.log(accounting);
