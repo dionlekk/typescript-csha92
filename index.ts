47dbbe97cfaa67ec46e5a41d215dc21a5e6a1a36
dionlekk <74821838+dionlekk@users.noreply.github.com>
2023-04-03 08:48:54 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -2,13 +2,13 @@
 import './style.css';
 
 class Department {
-  name: string;
-
-  constructor(n: string) {
-    this.name = n;
+  private employees : string[] = [];
+  constructor(private readonly id: string, public name: string) {
   }
+
+  describe() {}
+
+  addEmployee() {}
 }
 
-const accounting = new Department('Accounting');
-
-console.log(accounting);
+class ITDepartment extends Department {}
